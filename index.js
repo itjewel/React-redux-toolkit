5982661343b086c2bc95c9f35a94119c58c01bd6
Md.Jewel Mia <md.jewelmia52@gmail.com>
2022-07-03 09:24:49 +0600

    multiple reducere start

--- index.js
+++ index.js
@@ -4,34 +4,45 @@
 // subscribe() store connect to view
 const { createStore } = require("redux")
 // defining constants
-const ADD_USER = "ADD_USER";
+const GET_PRODUCTS = "GET_PRODUCTS";
+const ADD_PRODUCTS = "ADD_PRODUCTS";
 
 
 
 // state
 initialState = {
-    user: ["jewel"],
-    count: 1,
+    products: ["sugger", "salt"],
+    numberofProducts: 2,
 }
 
 // action -object- type
-const incrementUser = (value) => {
+const getProducts = () => {
     return {
-        type: ADD_USER,
-        payload: value,
+        type: GET_PRODUCTS,
+    };
+}
+const addProducts = (products) => {
+    return {
+        type: ADD_PRODUCTS,
+        payload: products,
     };
 }
 
 
 
-// reducer 
+// product reducer 
 
-const counterReducer = (state = initialState, action) => {
+const productReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_USER:
+        case ADD_PRODUCTS:
             return {
-                user: [...state.user, action.payload],
-                count: state.count + 1,
+                products: [...state.products, action.payload],
+                munber: state.numberofProducts + 1,
+            }
+
+        case GET_PRODUCTS:
+            return {
+                ...state,
             }
 
         default:
@@ -39,15 +50,15 @@ const counterReducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(counterReducer)
+const store = createStore(productReducer)
 
 store.subscribe(() => {
     console.log(store.getState())
 })
 
+store.dispatch(getProducts());
 
-store.dispatch(incrementUser("nadim"));
-store.dispatch(incrementUser("alian"));
-store.dispatch(incrementUser("saddam"));
+store.dispatch(addProducts("pen"));
+// store.dispatch(getProducts("nadim"));
 
 // run node index.js
