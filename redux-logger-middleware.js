3139a95492d992392df6586f0f9adeca277010c4
Md.Jewel Mia <md.jewelmia52@gmail.com>
2022-07-03 23:26:07 +0600

    redux logger middleware added

--- redux-logger-middleware.js
+++ redux-logger-middleware.js
@@ -10,50 +10,50 @@ const ADD_PRODUCTS = "ADD_PRODUCTS";
 
 // state products
 initialProductState = {
-    products: ["sugger", "salt"],
-    numberofProducts: 2,
+      products: ["sugger", "salt"],
+      numberofProducts: 2,
 }
 
 
 
 // action -object- type
 const getProducts = () => {
-    return {
-        type: GET_PRODUCTS,
-    };
+      return {
+            type: GET_PRODUCTS,
+      };
 }
 const addProducts = (products) => {
-    return {
-        type: ADD_PRODUCTS,
-        payload: products,
-    };
+      return {
+            type: ADD_PRODUCTS,
+            payload: products,
+      };
 }
 
 
 // product reducer 
 
 const productReducer = (state = initialProductState, action) => {
-    switch (action.type) {
-        case ADD_PRODUCTS:
-            return {
-                products: [...state.products, action.payload],
-                munber: state.numberofProducts + 1,
-            }
+      switch (action.type) {
+            case ADD_PRODUCTS:
+                  return {
+                        products: [...state.products, action.payload],
+                        munber: state.numberofProducts + 1,
+                  }
 
-        case GET_PRODUCTS:
-            return {
-                ...state,
-            }
+            case GET_PRODUCTS:
+                  return {
+                        ...state,
+                  }
 
-        default:
-            return state
-    }
+            default:
+                  return state
+      }
 }
 
 const store = createStore(productReducer, applyMiddleware(logger))
 
 store.subscribe(() => {
-    console.log(store.getState())
+      console.log(store.getState())
 })
 
 store.dispatch(getProducts());
