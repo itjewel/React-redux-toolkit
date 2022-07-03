e6e51481d38cdc245373c8769bc7e606f754d2f5
Md.Jewel Mia <md.jewelmia52@gmail.com>
2022-07-03 06:54:59 +0600

    node redux added

---
+++ index.js
@@ -0,0 +1,59 @@
+// store getState(), dispatch(), subcribe()
+// getState() see state data view here
+// dispatch() action for dispatch
+// subscribe() store connect to view
+const { createStore } = require("redux")
+// defining constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+
+// state
+initialState = {
+    count: 0,
+}
+
+// action -object- type
+const incrementCounter = () => {
+    return {
+        type: INCREMENT
+    };
+}
+
+const decrementCounter = () => {
+    return {
+        type: DECREMENT
+    };
+}
+
+// reducer 
+
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1,
+            }
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count - 1,
+            }
+
+        default:
+            return state
+    }
+}
+
+const store = createStore(counterReducer)
+
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+
+// run node index.js
